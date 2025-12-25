73df647c8cf7e179495d062207e53b598ad09a8c
hogiabao <hogiabao7725@gmail.com>
2025-12-25 23:45:51 +0700

    chore(fish): add fastfetch to starship init

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,7 +1,11 @@
 function fish_greeting
-    set_color brcyan
-    echo "╭─ Hi! Ho Gia Bao 🐟"
-    set_color bryellow
-    echo "╰─ What are you today... ✨"
-    set_color normal
+    if type -q fastfetch
+        fastfetch
+    else
+        set_color brcyan
+        echo "╭─ Hi! Ho Gia Bao 🐟"
+        set_color bryellow
+        echo "╰─ What are you today... ✨"
+        set_color normal
+    end
 end
