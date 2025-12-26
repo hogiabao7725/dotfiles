301e79a674f9abf710df8c7ddc51d87544406241
hogiabao <hogiabao7725@gmail.com>
2025-12-26 23:31:10 +0700

    chore(fish): rm fastfetch in vscode term

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,11 +1,13 @@
 function fish_greeting
-    if type -q fastfetch
-        fastfetch
-    else
-        set_color brcyan
-        echo "╭─ Hi! Ho Gia Bao 🐟"
-        set_color bryellow
-        echo "╰─ What are you today... ✨"
-        set_color normal
+    if test "$TERM_PROGRAM" != "vscode"
+        if type -q fastfetch
+            fastfetch
+        else
+            set_color brcyan
+            echo "╭─ Hi! $USER 🐟"
+            set_color bryellow
+            echo "╰─ What are you today... ✨"
+            set_color normal
+        end
     end
 end
