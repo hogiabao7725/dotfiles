40b9e3984461cf56d70c6b90a6dd919c83495877
Ho Gia Bao <hogiabao7725@gmail.com>
2026-05-14 23:55:41 +0700

    chore: ignore displaying gretting in code editor

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,13 +1,23 @@
 function fish_greeting
-    if test "$TERM_PROGRAM" != "vscode"
-        if type -q fastfetch
-            fastfetch
-        else
-            set_color brcyan
-            echo "╭─ Hi! $USER 🐟"
-            set_color bryellow
-            echo "╰─ What are you today... ✨"
-            set_color normal
-        end
+    # Skip inside editors
+    if test "$TERM_PROGRAM" = "vscode"
+        return
+    end
+
+    if test "$TERM_PROGRAM" = "zed"
+        return
+    end
+
+    # Skip non-interactive shell
+    status is-interactive; or return
+
+    if type -q fastfetch
+        fastfetch
+    else
+        set_color brcyan
+        echo "╭─ Hi! $USER 🐟"
+        set_color bryellow
+        echo "╰─ What are you today... ✨"
+        set_color normal
     end
 end
